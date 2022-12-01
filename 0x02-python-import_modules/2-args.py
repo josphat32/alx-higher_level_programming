33cedd298191f599d22bf2e4797a73acdfe64ee3
Josphat32 <mwangijosphat2030@gmail.com>
2022-12-01 04:30:14 -0800

    number of arg

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+    argc = len(argv)
+    if argc < 1:
+        print("{} arguments.".format(argc - 1))
+    else:
+        if argc == 1:
+            print("{} argument:".format(argc - 1))
+        else:
+            print("{} arguments:".format(argc - 1))
+            for n in range(1, argc):
+                print("{}: {}".format(n, argv[n]))
