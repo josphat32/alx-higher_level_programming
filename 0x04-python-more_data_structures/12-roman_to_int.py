84a131713dadccd0eb4b7d459e88ae31be516762
Josphat32 <mwangijosphat2030@gmail.com>
2023-01-05 04:21:23 -0800

    correction

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,36 +1,15 @@
 #!/usr/bin/python3
-
-def roman_value(prmCharacter):
-    roman_list = [('I', 1), ('V', 5), ('X', 10),
-                  ('L', 50), ('C', 100), ('D', 500), ('M', 1000)]
-
-    for item in roman_list:
-        character, value = item
-        if (prmCharacter is character):
-            return value
-    return None
-
-
-def next_value(prmString, prmIndex):
-    if prmIndex + 1 < len(prmString):
-        return roman_value(prmString[prmIndex + 1])
-    else:
-        return None
-
-
 def roman_to_int(roman_string):
-    result = 0
-
-    if (roman_string is None or isinstance(roman_string, str) is False):
-        return result
-
-    enum = enumerate(roman_string)
-    for index, character in enum:
-        currentValue = roman_value(character)
-        nextValue = next_value(roman_string, index)
-        if nextValue is None or currentValue >= nextValue:
-            result += currentValue
-        else:
-            result += (nextValue - currentValue)
-            next(enum)
-        return result
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    number = 0
+    if (type(roman_string) is not str or roman_string is None):
+       return (0)
+    for i, char in enumerate(roman_string):
+        tmp_num = roman[char]
+        try:
+            if tmp_num < roman[roman_string[i + 1]]:
+                tmp_num = tmp_num * -1
+        except IndexError:
+            pass
+        number = number + tmp_num
+    return (number)
