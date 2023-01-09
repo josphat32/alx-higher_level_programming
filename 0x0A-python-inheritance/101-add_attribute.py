9ca4a35feab71e03900c57a19ba12c2d09b32ab8
Josphat32 <mwangijosphat2030@gmail.com>
2023-01-09 12:05:01 -0800

    change attributes

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-def add_attribute(cls, name, first):
+def add_attribute(obj, att, value):
     '''
         adds new attribute
-    '''
+
     if not hasattr(cls, name):
         raise TypeError("can't add new attribute")
     cls.name = first
+    '''
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, att, value)
