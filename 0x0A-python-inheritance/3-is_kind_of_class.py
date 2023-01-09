67a5ed05ebbd411e583359674d8d975786126ee7
Josphat32 <mwangijosphat2030@gmail.com>
2023-01-09 12:22:55 -0800

    remove whitespace

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-    returns True if the object is an instance of, or if the object is an 
+    returns True if the object is an instance of, or if the object is an
     instance of a class that inherited from, the specified class;
     otherwise False
 '''
