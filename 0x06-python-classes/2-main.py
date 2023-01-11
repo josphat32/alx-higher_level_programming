bcacdf6d27cbd170b86c57474da6e57d9b879a5c
Josphat32 <mwangijosphat2030@gmail.com>
2023-01-11 00:45:55 -0800

    validate inputs

--- 0x06-python-classes/2-main.py
+++ 0x06-python-classes/2-main.py
@@ -5,7 +5,7 @@ my_square_1 = Square(3)
 print(type(my_square_1))
 print(my_square_1.__dict__)
 
-my_square_2 = Square(0)
+my_square_2 = Square()
 print(type(my_square_2))
 print(my_square_2.__dict__)
 
