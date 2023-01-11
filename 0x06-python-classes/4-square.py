a9063b3bc7553f85171b1ba7468a4f421e36f9ce
Josphat32 <mwangijosphat2030@gmail.com>
2023-01-11 01:31:43 -0800

    private attributes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -16,22 +16,22 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-        @property
-        def size(self):
-            """Get/set the current size of the square."""
-            return (self.__size)
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
 
-        @size.setter
-        def size(self, value):
-            """Updating the private attributes
-                Args:
-                    value (int): Zero or positve number.
-            """
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
-            elif value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+    @size.setter
+    def size(self, value):
+        """Updating the private attributes
+            Args:
+                value (int): Zero or positve number.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         """Return the current area of the square."""
