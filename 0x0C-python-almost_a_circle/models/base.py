b55320f960e5ec23add7e92e8d85bfeaf044576c
Josphat Ngugi <mwangijosphat2030@gmail.com>
2023-01-20 11:13:26 +0300

    add comments

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,6 +16,9 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        '''
+            Initiliases the class
+        '''
         if id is not None:
             self.id = id
         else:
