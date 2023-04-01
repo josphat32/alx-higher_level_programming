e23b0ce8660a3560c4eaafe5daa9c837d57a4620
Josphat32 <mwangijosphat2030@gmail.com>
2023-04-01 08:29:04 -0700

    corrections

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -17,5 +17,5 @@ if __name__ == '__main__':
             print("No result")
         else:
             print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
-    except:
+    except Exception:
         print("Not a valid JSON")
